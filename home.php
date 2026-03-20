0c4441f2dec15df02d47ac336cfc162efefc6162
Sofia Dos Santos <sofiaarantes1707@gmail.com>
2026-03-20 10:52:43 -0300

    Aula nova de pw

--- home.php
+++ home.php
@@ -4,91 +4,175 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/exemplo.css">
+    <title>PW2</title>
+    <link rel="stylesheet" href="css\exemplo.css">
 </head>
 
 <body>
-    <h1>Hello World</h1>
-    <h2>Variáveis e constantes PHP</h2>
-
+    <h2>
+        Variaveis e constantes PHP
+    </h2>
     <?php
-
-        //variável
-        /*
-
-        */
-        $a = 2; // int
-        $b = 0.0; // float
-        $c = ""; // string
-        $d = ''; // string
-        $e = null; // nulo
-        $teste = false; // boolean
-        
-        //constante
-        define("Minha_Const", "teste"); // "define" é uma função
-        const TESTE = "Valor";
-
-        //Escrevendo na tela
-        echo "<p>O valor de a é " . $a . "</p>\n"; // é possivel declarar tags apenas dentro do echo
-        echo "\t<p>O valor de b é $b</p>\n";
-        echo "\t<p>O valor de b é $b</p>\n";
-        print "\t<p>O valor de a + b é " . ($a + $b) . "</p>\n";
-
-        echo "O valor de " . TESTE;
-
-        //Objeto DateTime
-        $d = new DateTime(
-            "now",
-            new DateTimeZone(("America/Sao_Paulo"))
-        );
-
-        echo "\t<p>A data e hora atual é {$d->format("d/m/Y - H:i:s")}</p>\n";
-        // H -> a hora vai de 00 até 23, i -> minuto
-    ?>
-    <h1>Estruturas de decisão</h1>
-    <h2>IF</h2>
-    <?php
-        if ($a > $b) {
-            echo "\t<p>Deu verdadeiro</p>";
-        } else if ($a == $c && $b < $c) {
-            echo "\t<p>A é igaul a C";
-        }
-    ?>
-
-    <h2>SWITCH</h2>
-    <?php
-        switch ($a) {
-            case 1:
-                echo "\t<p>O valor é $a</p>\n";
-                break;
-            case 2:
-                echo "\t<p>O valor é $a</p>\n";
-                break;
-            case 3:
-                echo "\t<p>O valor é $a</p>\n";
-                break;
-            dafault:
-                echo !teste;
-                break;
-        }
-    ?>
-
-    <h1>Etruturs de repetição</h1>
-    <h2>Do...While</h2>
-    <?php
-        $i = 0;
-        do {
-            echo "O valor de i é $i";
-            i++;
-        } while($i <= 10);
-    ?>
-
+    //Variaveis
     
-    <h2>WHILE</h2>
+    $a = 2; //int
+    $b = 4.5; //floater
+    $c = ""; //string
+    $e = ''; //stirng
+    $d = null; //nulo
+    $teste = false; //bool 
+    
+    //constante
+    
+    define(constant_name: "minha_const", value: "teste");
+    const TESTE = "valor";
+
+
+    //escrevendo na tela
+    
+    echo "\t<p>O valor de \$a é " . $b . "</p>\n";
+    echo "\t<p>O valor de b é $a</p>\n";
+    echo '\t<p>O valor de b é ' . $b . '</p>\n';
+    print "\t<p>O valor de a + b é (773 + 4) = " . ($a + $b) . "</p>\n";
+
+    echo "O valor de " . TESTE;
+
+    //objeto datetime
+    
+    $d = new DateTime("now", new DateTimeZone("America/Sao_Paulo"));
+    echo "\t<p>A data atual é {$d->format("d/m/Y")} e são {$d->format("H:i:s")}</p>\n";
+    //ou ---> {$d->format("d/m/Y - h:i:s")}
+    ?>
+
+    <h2>Estruturas de Decisão</h2>
+    <h3>IF</h3>
+    <?php
+    if ($a > $b) {
+        echo "\t<p>Deu verdadeiro<p/>";
+    } elseif ($a == $c && $b < $c) {
+        echo "\t<p> A igual a C</p>";
+    }
+    ?>
+    <h3>SWITCH</h3>
+    <?php
+    $a = "5";
+    switch ($a) {
+        case 1:
+            echo "\t<p>O valor é 1!</p>\n";
+            break;
+        case 2:
+            echo "\t<p>O valor é 2!</p>\n";
+            break;
+        case 3:
+            echo "\t<p>O valor é 2!</p>\n";
+            break;
+        case 4:
+            echo "\t<p>O valor é 4</p>\n";
+            break;
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+            echo "\t<p>O valor está entre 5 e 8 </p>\n";
+            echo "\t<p>Confira o valor </p>\n";
+            break;
+        default:
+            echo "\t<p>O valor não está entre 1 e 8 </p>\n";
+            break;
+    }
+    ?>
+    <h2>Estruturas de Repetição</h2>
+
+    <h3>Do while</h3>
+    <?php
+    $i = 0;
+
+    do {
+        echo "\t<p>O valor de \$i é $i</p>\n";
+        $i++;
+    } while ($i <= 2);
+    ?>
+
+    <h2>While</h2>
     <?php
     $i = 0;
     while ($i <= 2) {
         echo "\t<p>O valor de \$i é $i</p>\n";
+        $i++;
+    }
+    ?>
+
+    <h2>For</h2>
+    <?php
+    for ($i = 0; $i <= 2; $i++) {
+        echo "\t<p>O valor de \$i é $i</p>\n";
+    }
+    ?>
+
+    <h2>Foreach</h2>
+    <?php
+    $vet = ["Zeca", "Pedreiro", "15998554984"];
+    $vet2 = array("Zeca", "Pedreiro", "15998554984", 123);
+    $vet2[] = "Itu";
+    array_push($vet, "SP", "1599758616");
+
+    foreach ($vet as $value) {
+        echo "\t<p>O valor atual é $value</p>\n";
+    }
+    ?>
+
+    <h2>For com vetor indexado</h2>
+    <?php
+
+    for ($i = 0; $i < count($vet2); $i++) {
+        echo "\t<p>O valor da posição " . $i + 1 . "º do vetor \$vet2 é {$vet2[$i]}</p>\n";
+    }
+    ?>
+
+    <h2>Foreach com vetor associativo</h2>
+    <?php
+    $vet_assoc = ["id" => "1", "nome" => "Tião", "telefone" => "15997586816"];
+    foreach ($vet_assoc as $key => $value) {
+        echo "\t<p>O valor da $key º do vetor \$vet_assoc é $value</p>\n";
+    }
+    echo "\t<p>Valor avulso de \$vet_assoc é {$vet_assoc["nome"]}</p>\n";
+    ?>
+
+    <h2>For com vetor bidmencional</h2>
+    <?php
+    // $cars = array(
+    //  array("Volvo", 22, 18),
+    //  array("BMW", 15, 13),
+    //  array("Saab", 5, 2),
+    //  array("Land Rover", 17, 15)
+    //);
+    $cars = [ 
+        ["Volvo", 22, 18],
+        ["BMW", 15, 13],
+        ["Saab", 5, 2, "me leva"],
+        ["Land Rover", 17, 15],
+        123
+    ];
+     echo "\t<p>Valor avulso de \$cars é {$cars["1"]["1"]}</p>\n";
+
+     for ($i=0; $i < count($cars); $i++) { 
+      if (is_array($cars[$i])) 
+        for ($j=0; $j < count($cars[$i]); $j++) { 
+             echo "\t<p>O valor da posição $j do vetor \$cars na posição \$j é {$cars[$i][$j]}</p>\n";
+        } 
+        else {
+            echo "\t<p>O valor da posição $i do vetor \$cars é {$cars[$i]}</p>\n";
+     }
+       } 
+    ?>
+
+    //VETOR bidmencional
+
+    <script src="js/bootstrap.bundle.js"></script>
+
+</body>
+
+</html>        echo "\t<p>O valor de \$i é $i</p>\n";
         $i++;
     }
     ?>
